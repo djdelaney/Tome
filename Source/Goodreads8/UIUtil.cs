99fbaaf837c0b735cbe5341ae6d94fb72df7cd42
Daniel Delaney <dan@hactar.com>
2012-10-10 21:36:52 -0400

    Added UI Util class

---
+++ Source/Goodreads8/UIUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
+
+namespace Goodreads8
+{
+    class UIUtil
+    {
+        public static void ShowToast(string message)
+        {
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText01);
+            var textXml = toastXml.GetElementsByTagName("text");
+            textXml.Item(0).InnerText = message;
+
+            // Create a toast from the Xml, then create a ToastNotifier object to show the toast.
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        public static async void ShowError(string message)
+        {
+            MessageDialog dialog = new MessageDialog(message);
+            await dialog.ShowAsync();
+        }
+    }
+}
